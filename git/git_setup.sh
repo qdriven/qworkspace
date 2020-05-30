ef28024e03c7903c010634330d4f4fad1c30b76c
patrick <patrickwuke@163.com>
2020-05-30 10:17:51 +0800

    add git setup scripts

--- git/git_setup.sh
+++ git/git_setup.sh
@@ -2,4 +2,6 @@
 
 # https://github.com/github/gitignore
 # https://github.com/JetBrains/idea-gitignore.git
-
+cd `pwd`
+curl https://gitee.com/qmeta/qworkspace/raw/master/git/.gitignore -o .gitignore
+curl https://gitee.com/qmeta/qworkspace/raw/master/git/.gitattributes -o .gitattributes
--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 echo `pwd`
 SCRIPT_HOME=`pwd`
 echo "alias gcm=\"sh $SCRIPT_HOME/git_commit.sh\"" >> ~/.zshrc
+
+echo "alias gs=\"sh $SCRIPT_HOME/git/git_setup.sh\"" >>~/.zshrc
+
+source ~/.zshrc
