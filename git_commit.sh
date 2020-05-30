8bcdd99b3bfe23075e85ca172f72fe5cb821c0d1
patrick <patrickwuke@163.com>
2020-05-30 10:12:58 +0800

    complete git commit remotes script

--- git_commit.sh
+++ git_commit.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+WORKSPACE=`pwd`
+cd ${WORKSPACE}
 git add .
 git commit -m "$1"
 
---
+++ install.sh
@@ -0,0 +1,5 @@
+# install scripts
+
+echo `pwd`
+SCRIPT_HOME=`pwd`
+echo "alias gcm=\"sh $SCRIPT_HOME/git_commit.sh\"" >> ~/.zshrc
