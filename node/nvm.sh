22a77aed6ede2257edccd62014ac0bfaf886ae38
Patrick <patrickwuke@163.com>
2021-11-20 16:22:08 +0800

    add node vm scripts

---
+++ node/nvm.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+## install nvm 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+ls ~/.nvm
+
+# add to profile
+# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+## install
+nvm install node
+nvm install <version_of_node>
+
+## list version
+nvm ls-remote
+
+nvm use node
+
+nvm install --lts
+nvm install 'lts/*' --reinstall-packages-from=current
+nvm install node --reinstall-packages-from=node
+
