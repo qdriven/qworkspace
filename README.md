727a96e4ccf9d0e7ca5f03c3267cc04036aacd79
Patrick <patrickwuke@163.com>
2022-02-05 18:15:45 +0800

    add new PowerToys

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ A repo to study and pratice improving productivity.
 - [hwo to jira plugins and scripts](docs/productivity/jira/jira-resources.md)
 
 ## Personal Scripts
+
 - [How to make own alias](qa-dotfiles/alias/slides.md)
 - [get-pid](script/get_pid) 打印pid
 - [获取git 仓库地址](script/gurl)
--- config/oh-my-zsh/zsh-installation.sh
+++ config/oh-my-zsh/zsh-installation.sh
@@ -5,6 +5,3 @@
 sh ./install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-ZSH_CUSTOM="/home/patrick/.oh-my-zsh/custom"
-## install zsh auto-suggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
