c81a826e7eacb2220d70cad9775c854139a1e525
Patrick <patrickwuke@163.com>
2022-03-27 23:20:12 +0800

    add pnpm install sripts

--- installation/languages/node/node-setup.sh
+++ installation/languages/node/node-setup.sh
@@ -7,6 +7,7 @@ curl -fsSL https://get.pnpm.io/install.sh | sh -
 npx pnpm add -g pnpm
 pnpm add -g pnpm
 
+pnpm config set store-dir ~/.pnpm-store
 
 yarn config set registry <>
 npm config set registry  <>
