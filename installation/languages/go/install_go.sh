8b8fb98d6cfc1bfa263da84948319c81a176ce05
Patrick <patrickwuke@163.com>
2022-03-27 19:10:28 +0800

    add install golang script in linux

--- installation/languages/go/install_go.sh
+++ installation/languages/go/install_go.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-VERSION="1.17.7"
-ARCH="amd64"
-curl -O -L "https://golang.org/dl/go${VERSION}.linux-${ARCH}.tar.gz"
+VERSION="1.18"
+# https://go.dev/dl/go1.18.linux-amd64.tar.gz
+ARCH="linux-amd64"
+curl -O -L "https://go.dev/dl/go{$VERSION}.{$ARCH}.tar.gz"
+# "https://go.org/dl/go${VERSION}.linux-${ARCH}.tar.gz"
+tar xvf go{$VERSION}.{$ARCH}.tar.gz
+mv go ~/go
 
+
+echo 'export PATH=$HOME/go/bin:$PATH' >> ~/.zshrc
+source ~/.zshrc
